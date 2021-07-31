30815287983d5c2e312bb6eb8ad51bba97e87ae3
Alekseev Artem <aalekseev@stream.ru>
2021-08-01 01:56:50 +0300

    Add initial Xcode project files

---
+++ MarleySpoonRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarleySpoonRecipes
+//
+//  Created by Artem Alekseev on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

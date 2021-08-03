1d400230edfea1efc562e12ea9e178d6389d107a
Alekseev Artem <aalekseev@stream.ru>
2021-08-03 15:43:23 +0300

    Integrate CocoaPods. Install PureLayout library

---
+++ Podfile
@@ -0,0 +1,8 @@
+# platform :ios, '9.0'
+
+target 'MarleySpoonRecipes' do
+  use_frameworks!
+
+  pod 'PureLayout', '~> 3.1.8'
+
+end
---
+++ Pods/Target Support Files/Pods-MarleySpoonRecipes/Pods-MarleySpoonRecipes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarleySpoonRecipes : NSObject
+@end
+@implementation PodsDummy_Pods_MarleySpoonRecipes
+@end

1e451349207de4cdb70573abcba216b7108ec00b
Alekseev Artem <aalekseev@stream.ru>
2021-08-03 15:00:06 +0300

    Refactor APIManager. Add loadData(for urlString:) method
    Create general completionHandler() func for handling completions from urlSession data task. Rename "completion: APIResult" to "callback: APIResult". Add loadData(for urlString:) method

--- MarleySpoonRecipes/Network/APIManager.swift
+++ MarleySpoonRecipes/Network/APIManager.swift
@@ -8,14 +8,16 @@
 import Foundation
 
 enum APIError: Error {
+	case wrongRequest
 	case parse
 	case server(String)
 }
 
-typealias APIResult = (Result<Data, APIError>) -> ()
+typealias APICallback = (Result<Data, APIError>) -> ()
 
 protocol APIManagerProtocol {
-	func getRecipes(completion: @escaping APIResult)
+	func getRecipes(callback: @escaping APICallback)
+	func loadData(for urlString: String, callback: @escaping APICallback)
 }
 
 private struct Endpoints {
@@ -46,39 +48,64 @@ class APIManager: APIManagerProtocol {
 
 	// MARK: - Functions
 
-	func getRecipes(completion: @escaping APIResult) {
+	func getRecipes(callback: @escaping APICallback) {
 		guard var urlComponents = URLComponents(string: entriesURLString) else {
+			callback(.failure(.wrongRequest))
 			return
 		}
 
 		urlComponents.query = "access_token=" + Credentials.accessToken
 
 		guard let url = urlComponents.url else {
+			callback(.failure(.wrongRequest))
 			return
 		}
 
 		let dataTask = urlSession.dataTask(with: url,
-									   completionHandler: { [weak self] data, response, error in
-										guard error == nil else {
-											self?.handle(.failure(.server(error?.localizedDescription ?? "")), completion)
-											return
-										}
-
-										guard let aResponse = response as? HTTPURLResponse,
-											  aResponse.statusCode == self?.okStatusCode,
-											  let aData = data else {
-											self?.handle(.failure(.parse), completion)
-											return
-										}
-
-										self?.handle(.success(aData), completion)
-									   })
+										   completionHandler: { [weak self] data, response, error in
+											self?.completionHandler(data, response, error, callback)
+										   })
 		dataTask.resume()
 	}
 
-	private func handle(_ result: Result<Data, APIError>, _ completion: @escaping APIResult) {
+	func loadData(for urlString: String, callback: @escaping APICallback) {
+		guard let url = URLComponents(string: urlString)?.url else {
+			callback(.failure(.wrongRequest))
+			return
+		}
+
+		let dataTask = urlSession.dataTask(with: url,
+										   completionHandler: { [weak self] data, response, error in
+											self?.completionHandler(data, response, error, callback)
+										   })
+		dataTask.resume()
+	}
+
+	// MARK: - Private Functions
+
+	private func completionHandler(_ data: Data?,
+								   _ response: URLResponse?,
+								   _ error: Error?,
+								   _ callback: @escaping APICallback) {
+		guard error == nil else {
+			let error = error?.localizedDescription ?? ""
+			handle(.failure(.server(error)), callback)
+			return
+		}
+
+		guard let aResponse = response as? HTTPURLResponse,
+			  aResponse.statusCode == okStatusCode,
+			  let aData = data else {
+			handle(.failure(.parse), callback)
+			return
+		}
+
+		handle(.success(aData), callback)
+	}
+
+	private func handle(_ result: Result<Data, APIError>, _ callback: @escaping APICallback) {
 		DispatchQueue.main.async {
-			completion(result)
+			callback(result)
 		}
 	}
 }

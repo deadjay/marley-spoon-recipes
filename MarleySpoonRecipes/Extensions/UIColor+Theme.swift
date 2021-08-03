1ecaf6f98360711858139b2ee3f2f582b72b1450
Alekseev Artem <aalekseev@stream.ru>
2021-08-03 15:31:38 +0300

    Reorganise files. Add UIColor+Theme extension

---
+++ MarleySpoonRecipes/Extensions/UIColor+Theme.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Theme.swift
+//  MarleySpoonRecipes
+//
+//  Created by Artem Alekseev on 03.08.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+	static var themeYellow = UIColor(displayP3Red: 252/255, green: 221/255, blue: 70/255, alpha: 1)
+	static var themeBlack = UIColor(displayP3Red: 33/255, green: 31/255, blue: 28/255, alpha: 1)
+	static var themeWhite = UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 1)
+	static var themeGray = UIColor(displayP3Red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+}
